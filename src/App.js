82201e32884200ee4457ce287f786402b5ff4cfd
Mateusz Lebioda <mateusz.lebioda.ml@gmail.com>
2021-08-13 20:47:56 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
